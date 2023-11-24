0f04f5dece163241eb3f13f0e449db3c34ca3fcb
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2023-11-23 22:28:33 -0300

    Update lombriz.c

--- lombriz.c
+++ lombriz.c
@@ -2,10 +2,10 @@
 //hay algun error al momento de reiniciar los valores de la partida
 
 #include <stdio.h>
-#include <conio.h>
+#include <conio.h>//con esta libreria podemos usar las funciones como getch o kbhit
 #include <stdlib.h>
 #include <stdbool.h>
-#include <windows.h>
+#include <windows.h>//utilizo esta libreria para poder usar la funcion sleep y controlar la velocidad del juego
 #include "lombriz.h"
 
 
